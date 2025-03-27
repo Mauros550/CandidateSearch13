bb8a1dec0ee18b8a4821900907350650bb2663fe
mauro mariscal <mariscalmauro0608@gmail.com>
2025-03-27 16:56:22 -0700

    newcode

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,13 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+
+interface Candidate {
+    login: string; 
+    name?: string;
+    avatar_url: string;
+    email?: string;
+    location?: string;
+    html_url: string;
+    company?: string;
+  }
+  
+  export default Candidate;
+  
